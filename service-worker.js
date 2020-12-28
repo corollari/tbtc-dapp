9b6d7b937d7a214435cd3af480838a9ef944e885
corollari <corollari@users.noreply.github.com>
2020-12-28 03:14:51 +0000

    deploy: 48e1cfa6b4f8588d93fec180273ee5a6e579ee2a

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tbtc-dapp/precache-manifest.931c37db87d0247e7131ce4a3db3364e.js"
+  "/precache-manifest.652fa84f72db32d8cd1b36e0b1884b84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tbtc-dapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
